94c76cc405a427e459ac07d38dd65eb0ba13fa69
Douglas Breaker <dbreaker@douglas-breakers-macbook-pro-3.local>
2011-05-10 09:21:39 -0500

    search page improvements

--- app/controllers/kits_controller.rb
+++ app/controllers/kits_controller.rb
@@ -4,6 +4,8 @@ class KitsController < ApplicationController
     slug = params[:slug] || "large-2-bedroom"
     session[:size_slug] = slug
     @move_size = MoveSize.find_by_slug(slug)
+    @search = Search.new()
+    @search.move_size = @move_size
     @meta_title = "#{@move_size.home_size} Moving Box Kits | Moving Boxes for a #{@move_size.home_size} | MovingBoxSearch.com"
     @meta_desc = "#{@move_size.home_size} Moving Box Kits - Compare prices from multiple providers.  Buy online and get your boxes in a couple days.  Free Shipping. MovingBoxSearch.com"
   end
